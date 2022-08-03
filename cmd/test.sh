63943f7181d6114d80855b30647bb706960ab334
Final233 <23329853+Final233@users.noreply.github.com>
2022-08-03 11:07:38 +0800

    2022-08-03-11:07:38

---
+++ cmd/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#********************************************************************
+#Author:                Final
+#QQ:                    438803792
+#Date:                  2022-08-03
+#FileName：             test.sh
+#URL:                   http://cnblogs.com/fina
+#Description：          The test script
+#Copyright (C):         2022 All rights reserved
+#********************************************************************
+
+basepath=$(
+    cd $(dirname $0)
+    pwd
+)
+
+cd $basepath/cmd
---
+++ mod_time.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#
+#********************************************************************
+#Author:                Final
+#FileName：             mod_time.sh
+#Copyright (C):         2022 All rights reserved
+#********************************************************************
+
+#find /home/tdgw -name 'MTP-Pbu.json' | xargs grep knockDoorInAdvanceMs
+#find /home/tdgw/ -name 'MTP-Pbu.json' | xargs grep pbu
+#find /home/tdgw/AUTOF* -name 'MTP-Pbu.json'
+#sed -rn '/knockDoorInAdvanceMs/s@(.* ).*,@\188,@p'  MTP-Pbu.json
+#grep 'pbu"' MTP-Pbu.json | tr -d '":,' | awk '{print $NF}'
+basepath=$(
+    cd $(dirname $0)
+    pwd
+)
+
+cd $basepath
+DATE=$(date +%Y%m%d)
+DATE1=$(date +%Y%m%d-%H%M%S)
+_run() {
+    file_name='MTP-Pbu.json'
+    if [ $# -ge 2 ]; then
+        file_list=$(find /home/tdgw/AUTOF* -name ${file_name})
+        time=$2
+        for file_path in $file_list; do
+            run_path=${file_path/cfg\/${file_name}/}
+            pbu=$(grep 'pbu"' $file_path | tr -d '":,' | awk '{print $NF}')
+            if [ "$1" -eq "$pbu" ]; then
+                [ -d $basepath/${DATE}_bak ] || mkdir -p $basepath/${DATE}_bak
+                cp -a $file_path $basepath/${DATE}_bak/${file_name}_bak_${DATE1}_${pbu}${file_path////_}
+                # cd $run_path && bash shutdown.sh
+                sed -ri "/knockDoorInAdvanceMs/s@(.* ).*,@\1"${time:=100}",@" $file_path && echo "$pbu mod_time is succ"
+                # cd $run_path && bash startup.sh
+            fi
+        done
+    elif [ "$1" == "list" ]; then
+        # file_list=$(find /home/tdgw/AUTOF* -name ${file_name} | xargs grep 'pbu"' | tr -d '":,' | awk '{print $1,$NF}')
+        # file_time=$(find /home/tdgw/AUTOF* -name ${file_name} | xargs grep 'knockDoorInAdvanceMs' | awk '{print $1,$NF}' | tr -d ,)
+        file_path=$(find /home/tdgw/AUTOF* -name ${file_name})
+        for i in $file_path; do
+            pbu=$(grep 'pbu"' $i | tr -d '":,' | awk '{print $NF}')
+            time=$(awk '/knockDoorInAdvanceMs/{print $NF}' $i | tr -d ,)
+            echo $i $pbu $time
+        done
+
+    else
+
+        echo -e "
+注：脚本请放入 /home/tdgw/scripts 目录下执行
+Usage: bash command [options] [args] 
+
+Samples:
+    bash $0 list # 查看
+    bash $0 12345 88 # PBU [NUM(1-100)]
+==============================================================================
+\n"
+    fi
+}
+
+_run "$@"
