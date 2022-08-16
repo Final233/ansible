edc4e2084471b7c13013084cc7ba3fef0aa7ea73
Final233 <23329853+Final233@users.noreply.github.com>
2022-08-16 13:19:07 +0800

    2022-08-16-13:19:06

--- cmd/upload.sh
+++ cmd/upload.sh
@@ -12,14 +12,12 @@ basepath=$(
 )
 
 cd $basepath/../
-
 DATE=$(date +%F-%T)
-# rm -f bak-*.tar.gz
-rm -f logs/*.log
-# git remote set-url origin https://xxxxxxx.com/cangku
-git rm -r --cached
+rm -f logs/*.log files/*
+
+# # git rm -r --cached
 git add -A
 git commit -m "$DATE"
 git push && echo 上传成功
-#DATE=$(date +%Y%m%d)
-#tar zcf bak-${DATE}.tar.gz * && echo 备份完成
+git gc
+
